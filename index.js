1f4db98d2f33b6793328e96e34efbc54ed4cd7d5
Sachin-stha112 <coder.billionaire@gmail.com>
2026-08-31 10:09:12 +0545

    ES module

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const math = require("./math")
-console.log(math.add(5,5))
-console.log(math.sub(10,5))
+// const math = require("./math")
+// console.log(math.add(5,5))
+// console.log(math.sub(10,5))
+import getPosts from "./postController.js";
+console.log(getPosts());
---
+++ postController.js
@@ -0,0 +1,6 @@
+const posts = [
+    {id :1, title : 'Flood'},
+    {id :2, title : 'Oscar Awards'}
+]
+const getPosts = () => posts
+export default getPosts
