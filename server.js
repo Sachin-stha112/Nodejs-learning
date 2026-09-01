af63f549a7c358d09b4465a8de0f3d1a498c8fc1
Sachin-stha112 <coder.billionaire@gmail.com>
2026-09-01 09:09:32 +0545

    implement PORT from .env in server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import http from "http";
+import "dotenv/config";
 
-const server = http.createServer((req,res) => {
-    res.end("Hello World")
-})
-server.listen(3000)
+const server = http.createServer((req, res) => {
+  res.end("<h1>Hello how is weather World</h1>");
+});
+/* Using PORT from env or in case missing falling back to port 3000 */
+const PORT = process.env.PORT || 3000;
+server.listen(PORT);
