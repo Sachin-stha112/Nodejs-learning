de6bc39a7d714dd67a6bfd846f8e652ab7b2b89d
Sachin-stha112 <coder.billionaire@gmail.com>
2026-09-02 11:45:44 +0545

    Simple ROuting accepting GET req only

--- server.js
+++ server.js
@@ -2,21 +2,33 @@ import http from "http";
 import "dotenv/config";
 
 const server = http.createServer((req, res) => {
-    /* Simple ROuting */
-    if(req.url === '/')
-    {
-        res.writeHead(200, { "content-type": "text/html" });
-        res.end("<h1>Home Page</h1>");
-    }
-    else if(req.url === '/about')
-    {
-        res.writeHead(200, { "content-type": "text/html" });
-        res.end("<h1>ABout Page</h1>");
-    }
-    else
-    {
-        res.writeHead(404, { "content-type": "text/plain" });
-        res.end("Page not found");
+    /* Simple ROuting accepting GET req only*/
+    try {
+        if (req.method === 'GET')
+        {
+            if (req.url === "/") {
+              res.writeHead(200, { "content-type": "text/html" });
+              res.end("<h1>Home Page</h1>");
+            } else if (req.url === "/about") {
+              res.writeHead(200, { "content-type": "text/html" });
+              res.end("<h1>ABout Page</h1>");
+            } else {
+              res.writeHead(404, { "content-type": "text/plain" });
+              res.end("Page not found");
+            }
+        }
+        else
+        {
+            throw new Error("Method not allowed")
+        }
+    } catch (error) {
+        if (error.message === "Method not allowed") {
+          res.writeHead(405, { "content-type": "text/plain" });
+          res.end("Method not allowed");
+        } else {
+          res.writeHead(500, { "content-type": "text/plain" });
+          res.end("Server Error");
+        }
     }
 
 
