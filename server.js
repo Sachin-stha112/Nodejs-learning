b5d62863205299670fb099750dcf57b36ac370b3
Sachin-stha112 <coder.billionaire@gmail.com>
2026-09-02 10:44:46 +0545

    handle incoming requests with req/res objects

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ import http from "http";
 import "dotenv/config";
 
 const server = http.createServer((req, res) => {
-  res.end("<h1>Hello how is weather World</h1>");
+  console.log(req.url);
+  console.log(req.method);
+
+  res.writeHead(200, {"content-type" : "text/html"})
+  res.end("<h1>Hello World</h1>")
 });
 /* Using PORT from env or in case missing falling back to port 3000 */
 const PORT = process.env.PORT || 3000;
