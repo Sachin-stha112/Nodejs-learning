bc428d9d0acfe0f8141e6bf8867c045ef99b1b69
Sachin-stha112 <coder.billionaire@gmail.com>
2026-09-07 09:09:52 +0545

    fix: wrap readFile in async function with try/catch for proper error handling

--- fsDemo.js
+++ fsDemo.js
@@ -1,5 +1,16 @@
 import fs from 'node:fs/promises'
-console.log("Start")
-const data = await fs.readFile("demo.txt", "utf8")
-console.log(data)
-console.log("END")
+
+async function readFileDemo()
+{   
+    /* Reading file must be inside try and catch block, coz sometimes we might get error while reading the file */
+    try
+    {
+        const data = await fs.readFile('demo.txt', 'utf8')
+        console.log(data);
+    }
+    catch(err)
+    {
+        console.error("Error reading the file: ", err);
+    }
+}
+readFileDemo();
