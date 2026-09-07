eb2cf0f716cd26b77f3e3599da0f026bbd255bdb
Sachin-stha112 <coder.billionaire@gmail.com>
2026-09-07 09:26:20 +0545

    feat: add writeFile demo and update readFile to use created file

--- fsDemo.js
+++ fsDemo.js
@@ -1,11 +1,24 @@
 import fs from 'node:fs/promises'
 
+async function writeFileDemo()
+{
+    try
+    {
+        await fs.writeFile('demo2.txt', 'Hello World', 'UTF8')
+        console.log("File created successfully!")
+    }
+    catch (err)
+    {
+        console.error("Error while Creating file: ", err)
+    }
+}
+
 async function readFileDemo()
 {   
     /* Reading file must be inside try and catch block, coz sometimes we might get error while reading the file */
     try
     {
-        const data = await fs.readFile('demo.txt', 'utf8')
+        const data = await fs.readFile('demo2.txt', 'utf8')
         console.log(data);
     }
     catch(err)
@@ -13,4 +26,5 @@ async function readFileDemo()
         console.error("Error reading the file: ", err);
     }
 }
+writeFileDemo();
 readFileDemo();
