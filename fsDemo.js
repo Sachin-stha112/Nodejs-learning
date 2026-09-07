d4831c19384663e431f454751dc4d3297fd94219
Sachin-stha112 <coder.billionaire@gmail.com>
2026-09-07 09:49:51 +0545

    feat: add write/read JSON file demo with JSON.parse

--- fsDemo.js
+++ fsDemo.js
@@ -1,30 +1,42 @@
 import fs from 'node:fs/promises'
 
-async function writeFileDemo()
-{
-    try
-    {
-        await fs.writeFile('demo2.txt', 'Hello World', 'UTF8')
-        console.log("File created successfully!")
-    }
-    catch (err)
-    {
-        console.error("Error while Creating file: ", err)
+// async function writeFileDemo()
+// {
+//     try
+//     {
+//         await fs.writeFile('demo2.txt', 'Hello World', 'UTF8')
+//         console.log("File created successfully!")
+//     }
+//     catch (err)
+//     {
+//         console.error("Error while Creating file: ", err)
+//     }
+// }
+
+async function writeJSONDemo() {
+    try {
+        const data = JSON.stringify({
+            name: 'Sachin',
+            age: 22,
+            email: 'sachin@goat.com',
+        })
+       await fs.writeFile('data.json', data, 'utf8')
+
+    } catch (err) {
+        console.error("Error while creating file: ", err);
     }
 }
 
-async function readFileDemo()
-{   
+async function readFileDemo() {
     /* Reading file must be inside try and catch block, coz sometimes we might get error while reading the file */
-    try
-    {
-        const data = await fs.readFile('demo2.txt', 'utf8')
-        console.log(data);
-    }
-    catch(err)
-    {
-        console.error("Error reading the file: ", err);
+    try {
+        const data = await fs.readFile('data.json', 'utf8')
+        const jsonData = JSON.parse(data)
+        console.log(jsonData)
+    } catch (err) {
+        console.error('Error reading the file: ', err)
     }
 }
-writeFileDemo();
-readFileDemo();
+// writeFileDemo()
+await writeJSONDemo()
+readFileDemo()
