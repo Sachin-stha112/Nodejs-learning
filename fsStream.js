b35702c50b188d35e28fd8874727efa31e795a42
Sachin-stha112 <coder.billionaire@gmail.com>
2026-09-08 18:39:48 +0545

    updated: highWaterMark (Controlling Threshold of Chunks)

--- fsStream.js
+++ fsStream.js
@@ -1,21 +1,22 @@
 import fs from 'node:fs'
-let totalBytes = 0;
+let totalBytes = 0
 
-const stream = fs.createReadStream ('demo.txt', {
-    encoding : 'utf8'
+const stream = fs.createReadStream('demo.txt', {
+    encoding: 'utf8',
+    highWaterMark: 10,
 })
-// doing things in some events like data, error, end
+// highWaterMark (Controlling Threshold of Chunks)
 stream.on('data', (chunk) => {
-    console.log("Data Received: ")
+    console.log('Data Received: ')
     console.log('Chunk Size: ', chunk.length, ' bytes')
-    console.log('Chunk Length Meter: ', '='.repeat(Math.ceil(chunk.length / 10)))
+
     console.log(chunk)
     totalBytes += chunk.length
 })
 stream.on('end', () => {
-    console.log("Finished Reading")
-    console.log("Total Bytes: ", totalBytes)
+    console.log('Finished Reading')
+    console.log('Total Bytes: ', totalBytes)
 })
 stream.on('error', (error) => {
-    console.error("Something went wrong: ", error)
+    console.error('Something went wrong: ', error)
 })
