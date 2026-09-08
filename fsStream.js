04af1335bc77c12eba28a0019f0801fbe1d74bbd
Sachin-stha112 <coder.billionaire@gmail.com>
2026-09-08 14:46:45 +0545

    feat: add chunk length meter to fsStream

--- fsStream.js
+++ fsStream.js
@@ -1,4 +1,5 @@
 import fs from 'node:fs'
+let totalBytes = 0;
 
 const stream = fs.createReadStream ('demo.txt', {
     encoding : 'utf8'
@@ -6,10 +7,14 @@ const stream = fs.createReadStream ('demo.txt', {
 // doing things in some events like data, error, end
 stream.on('data', (chunk) => {
     console.log("Data Received: ")
+    console.log('Chunk Size: ', chunk.length, ' bytes')
+    console.log('Chunk Length Meter: ', '='.repeat(Math.ceil(chunk.length / 10)))
     console.log(chunk)
+    totalBytes += chunk.length
 })
 stream.on('end', () => {
     console.log("Finished Reading")
+    console.log("Total Bytes: ", totalBytes)
 })
 stream.on('error', (error) => {
     console.error("Something went wrong: ", error)
