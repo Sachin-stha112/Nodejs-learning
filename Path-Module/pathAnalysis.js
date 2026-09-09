75de326b8d33e0011eb03b56b68935063d18e3f1
Sachin-stha112 <coder.billionaire@gmail.com>
2026-09-09 14:35:27 +0545

    feat: add dirname() example in pathAnalysis.js

--- Path-Module/pathAnalysis.js
+++ Path-Module/pathAnalysis.js
@@ -6,3 +6,5 @@ console.log("Basename: ", path.basename(filePath))
 // basename("filePath", ".ext") : Get filename without extension
 console.log("Basename without extension: ", path.basename(filePath, ".txt"))
 
+// dirname() : Give you everything other than basename
+console.log("Directory name: ", path.dirname(filePath))
