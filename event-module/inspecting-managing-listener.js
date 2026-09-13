f86d8559ddaed2cc29bde9fbe206a9e082aeeab0
Sachin-stha112 <coder.billionaire@gmail.com>
2026-09-13 23:09:47 +0545

    feat: add listener inspection example and fix file naming

---
+++ event-module/inspecting-managing-listener.js
@@ -0,0 +1,24 @@
+import { EventEmitter } from 'node:events'
+
+const emitter = new EventEmitter()
+
+function handleLogin()
+{
+    console.log("Log in handled")
+}
+function logLogin()
+{
+    console.log("Login Logged")
+}
+function sendNotification()
+{
+    console.log("notification-sent")
+}
+//
+emitter.on('login', handleLogin)
+emitter.on('login',logLogin)
+emitter.on('login', sendNotification)
+// Inspection section
+console.log("Number of Listeners: ", emitter.listenerCount('login'))
+
+console.log('List of Listeners: ', emitter.listeners('login'))
