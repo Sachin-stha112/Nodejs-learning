8b6643542b263743783bf88c18b179791a0b500d
Sachin-stha112 <coder.billionaire@gmail.com>
2026-09-13 23:14:28 +0545

    feat: add eventNames and maxListeners inspection examples

--- event-module/inspecting-managing-listener.js
+++ event-module/inspecting-managing-listener.js
@@ -2,23 +2,24 @@ import { EventEmitter } from 'node:events'
 
 const emitter = new EventEmitter()
 
-function handleLogin()
-{
-    console.log("Log in handled")
+function handleLogin() {
+    console.log('Log in handled')
 }
-function logLogin()
-{
-    console.log("Login Logged")
+function logLogin() {
+    console.log('Login Logged')
 }
-function sendNotification()
-{
-    console.log("notification-sent")
+function sendNotification() {
+    console.log('notification-sent')
 }
 //
 emitter.on('login', handleLogin)
-emitter.on('login',logLogin)
+emitter.on('login', logLogin)
 emitter.on('login', sendNotification)
 // Inspection section
-console.log("Number of Listeners: ", emitter.listenerCount('login'))
-
+console.log('Number of Listeners: ', emitter.listenerCount('login'))
 console.log('List of Listeners: ', emitter.listeners('login'))
+
+console.log('List of events: ', emitter.eventNames())
+// managing Listener
+emitter.setMaxListeners(1)
+console.log(emitter.getMaxListeners())
